09315c8f7fc2434babf65de622d7e7f2f082997a
Lukas Holthof <lukas.holthof@sap.com>
2024-04-12 12:19:44 +0200

    Update startConnectors.bat

--- startConnectors.bat
+++ startConnectors.bat
@@ -1,4 +1,8 @@
 call gradlew clean edc-controlplane:edc-runtime-memory-dim:build
+call gradlew clean bdrsmock:build -x checkstyleMain
+
+echo "### Starting BDRS Mock"
+start java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:8092 -jar bdrsmock/build/libs/bdrsmock-image.jar
 
 echo "### Starting Consumer Connector"
 set EDC_FS_CONFIG=edc-controlplane/edc-runtime-memory-dim/consumer-configuration.properties
